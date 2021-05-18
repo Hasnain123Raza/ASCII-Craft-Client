23fd2875b6977ee40b8ca7af859a7ac6673d5ca0
Hasnain Raza <hasnain.mohsan@gmail.com>
2021-05-18 11:57:46 -0400

    Added Create scene gui

--- src/areas/Art/scenes/Create/index.js
+++ src/areas/Art/scenes/Create/index.js
@@ -1,3 +1,49 @@
+import { Container, Form, Button, Card } from "react-bootstrap";
+
 export default function () {
-  return <div className="art-create">Create</div>;
+  return (
+    <div className="art-create">
+      <Container className="my-4">
+        <h2 className="mb-3" style={{ "text-align": "center" }}>
+          ASCII-CRAFT Art Editor
+        </h2>
+
+        <Card>
+          <Card.Body>
+            <Form>
+              <Form.Group controlId="title">
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="text" placeholder="Title" />
+                <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group controlId="description">
+                <Form.Label>Description</Form.Label>
+                <Form.Control type="text" placeholder="Description" />
+                <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group controlId="content">
+                <Form.Label>Content</Form.Label>
+                <Form.Control
+                  autoComplete="off"
+                  as="textarea"
+                  rows={3}
+                  placeholder="Content"
+                />
+                <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+              </Form.Group>
+            </Form>
+          </Card.Body>
+        </Card>
+
+        <div className="my-4 d-flex">
+          <Button variant="primary">New</Button>
+          <Button className="ml-auto" variant="success">
+            Create
+          </Button>
+        </div>
+      </Container>
+    </div>
+  );
 }
